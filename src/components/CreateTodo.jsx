48da6f590e9345eb32df6584efd954f2c925def7
Sai Sawant <saisawant2003@gmail.com>
2023-05-22 21:41:30 +0530

    new feature of adding me new todo added

--- src/components/CreateTodo.jsx
+++ src/components/CreateTodo.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-
+import React, { useRef } from 'react'
+import { useDispatch } from 'react-redux'
+import {addTodo} from "../store/slice/TodoSlice"
 const CreateTodo = () => {
+  const todoRef=useRef()
+  const dispatch=useDispatch()
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    dispatch(addTodo(todoRef.current.value))
+    todoRef.current.value=""
+  }
   return (
     <div>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <input
+            ref={todoRef}
             className="w-[100%] mt px-8 mt-[50px] text-[18px] h-[62px] rounded-lg"
             type="text"
             placeholder="Create a new Todo.."
--- src/store/slice/TodoSlice.js
+++ src/store/slice/TodoSlice.js
@@ -1,21 +1,14 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+import { v4 as uuidv4 } from 'uuid';
 const TodoSlice=createSlice({
-    initialState:[{id:1,todo:"complete maths homework",status:"completed"},
-    {id:2,todo:"complete maths homework",status:"incomplete"},
-    {id:3,todo:"complete maths homework",status:"completed"},
-    {id:4,todo:"complete maths homework",status:"incomplete"},
-    {id:5,todo:"complete maths homework",status:"completed"},
-    {id:6,todo:"complete maths homework",status:"completed"},
-    {id:7,todo:"complete maths homework",status:"completed"},
-    {id:8,todo:"buy ice cream ",status:"completed"},
-    {id:9,todo:"drop harry to school",status:"completed"},
-    {id:10,todo:"complete maths homework",status:"completed"}
-],
+    initialState:[],
     name:'todo',
     reducers:{
         addTodo:(state,action)=>{
-            state.push(action.payload)
+            const newTodo= {id:uuidv4(),
+                todo:action.payload,
+                status:"unChecked"}
+            state.push(newTodo)
         },
     }
 })
