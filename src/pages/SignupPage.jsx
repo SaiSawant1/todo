cbdd48247ec1b502bc88090987037a472dac36ab
Sai Sawant <saisawant2003@gmail.com>
2023-05-24 23:08:07 +0530

    sign up page styling is completed

--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -6,28 +6,28 @@ import signUpImage from "../images/signUpImage.jpg";
 const SignupPage = () => {
   return (
     <div className="signupPage flex justify-between items-center h-[100vh] w-[100vw]">
-      <div className="signup-wrapper flex flex-col h-[100vh] w-[50%] justify-center items-center">
-        <div className="signupForm w-[70%] h-[70%] text-center">
-          <h1 className="text-5xl m-8" >Sign up</h1>
+      <div className="signup-wrapper  flex flex-col h-[100vh] w-[50%] justify-center items-center">
+        <form className="signupForm p-6 rounded-md w-[70%] h-[60%] text-center">
+          <h1 className="text-5xl m-3" >Sign up</h1>
           <div>
             <h2 className="text-3xl m-2">Hello!</h2>
-            <p className="text-xl m-1">Sign in to your account</p>
+            <p className="text-xl m-1">Enter Information to create your new account</p>
           </div>
-          <div>
+          <div className="mt-3 w-full flex h-10 items-center p-2 text-lg bg-white rounded-md ">
             <FontAwesomeIcon icon={faEnvelope} />
-            <input type="email" placeholder="Enter Email" name="" id="" />
+            <input className="ml-2 grow" type="email" placeholder="Enter Email" name="" id="" />
           </div>
-          <div>
-            <FontAwesomeIcon icon={faLock} />
-            <input type="password" placeholder="Enter Password" />
+          <div className="mt-3 w-full flex h-10 items-center p-2 text-lg bg-white rounded-md ">
+            <FontAwesomeIcon  icon={faLock} />
+            <input className="ml-2 grow" type="password" placeholder="Enter Password" />
           </div>
-          <div>
-            <FontAwesomeIcon icon={faLock} />
-            <input type="password" placeholder="Confirm Password" />
-            <FontAwesomeIcon icon={faEye} />
+          <div className="mt-3 w-full flex h-10 items-center p-2 text-lg bg-white rounded-md ">
+            <FontAwesomeIcon  icon={faLock} />
+            <input className="ml-2 grow" type="password" placeholder="Confirm Password" />
+            <FontAwesomeIcon  icon={faEye} />
           </div>
-          <button>Sign up</button>
-        </div>
+          <button className="mt-3 rounded-2xl  w-full h-10 text-lg ">Sign up</button>
+        </form>
       </div>
 
       <div className="signupImage h-[100vh] w-[50%]">
